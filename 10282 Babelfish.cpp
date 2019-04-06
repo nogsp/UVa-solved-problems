521a3a6580b3af6d3c9e69efb0263d1ea34933cf
Pedro Nogueira Coutinho <49210182+P-N-C@users.noreply.github.com>
2019-04-06 18:00:18 -0300

    Update 10282 Babelfish.cpp

--- 10282 Babelfish.cpp
+++ 10282 Babelfish.cpp
@@ -6,7 +6,7 @@ int main(void){
     string s;
     int i;
     map <string, string> mapa;
-    while(getline(cin,s)){
+    while(getline(cin,s), s!=""){
         int tam = s.size(), op=0;
         string s1,s2;
         for(i=0;i<tam;i++){
@@ -17,11 +17,11 @@ int main(void){
             if(!op) s1+=s[i];
             else s2+=s[i];
         }
-        mapa[s2]=s1;       
+        mapa[s2]=s1;
+        
     }
     while(cin >> s){
-        if(mapa.count(aux)<=0) cout << "eh" << endl;
+        if(mapa.count(s)<=0) cout << "eh" << endl;
         else cout << mapa[s] << endl;
     }
-    cout << endl;
 }
