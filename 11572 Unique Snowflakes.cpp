9d9b703b1788d853b29b7ac344d8e3fee9695839
Pedro Nogueira Coutinho <49210182+P-N-C@users.noreply.github.com>
2019-05-17 22:59:28 -0300

    Update 11572 Unique Snowflakes.cpp

--- 11572 Unique Snowflakes.cpp
+++ 11572 Unique Snowflakes.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#define pb push_back
+using namespace std;
 
+int main(){
+	int t;
+	scanf("%d", &t);
+	while(t--){
+		int n;
+		scanf("%d", &n);
+		set <long long int> myset;
+		vector <long long int> vec;
+		for(int i=0;i<n;i++){
+			long long int snow;
+			scanf("%lld", &snow);
+			vec.pb(snow);
+		}
+		long long int resp=0, cont=0, p=0, q=0;
+		while(q<n){
+			if(!myset.count(vec[q])) myset.insert(vec[q++]);
+			else{
+				myset.erase(vec[p++]);
+			}
+			resp = max(resp, q-p);
+		}
+		cout << resp << endl;
+			
+	}
+	return 0;
+}
