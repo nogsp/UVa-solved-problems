229d04c659b43b5b084fea06adda569067c98415
Pedro Nogueira Coutinho <49210182+P-N-C@users.noreply.github.com>
2019-05-26 12:06:03 -0300

    Create 10815 Andy's First Dictionary.cpp

---
+++ 10815 Andy's First Dictionary.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#define pb push_back
+using namespace std;
+
+int main(){
+	string line;
+	set <string> myset;
+
+	while(getline(cin, line)){
+		string buffer="";
+		for(int i=0, sz=line.size();i<sz;i++){
+			if(isalpha(line[i])){
+				buffer += line[i];
+				if(i<sz-1) continue;
+			}	
+			if(buffer!=""){
+				for(int j=0;j<buffer.size();j++) 
+					if(buffer[j]>='A' && buffer[j]<='Z') buffer[j] += 32;
+				if(myset.count(buffer)==0){
+					myset.insert(buffer);
+				}	
+				buffer = "";
+			}
+		}
+	}
+	for(auto it = myset.begin();it != myset.end();it++){
+		cout << *it << endl;
+	}
+	return 0;
+}
