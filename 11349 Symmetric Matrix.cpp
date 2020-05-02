8d5887d1fb686a99cc086a75d6b25dc809cb095f
Pedro Nogueira Coutinho <49210182+P-N-C@users.noreply.github.com>
2020-05-02 19:39:16 -0300

    Create 11349 Symmetric Matrix.cpp

---
+++ 11349 Symmetric Matrix.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define pb push_back
+#define ll long long
+ 
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    ll t, casos = 0;
+    cin >> t;
+    while(t--) {
+        string s;
+        ll n;
+        cin >> s >> s >> n;
+        vector<vector<ll>> mat(n, vector<ll>(n, 0));
+        bool key = true;
+        for(ll i = 0; i < n; i++) {
+            for(ll j = 0; j < n; j++) {
+                cin >> mat[i][j];
+                if(mat[i][j] < 0) key = false;
+            }
+        }
+        
+        for(ll i = 0; i < n; i++) {
+            if(!key) break;
+            for(ll j = 0; j < n; j++) {
+                if(mat[i][j] != mat[n - i - 1][n - j - 1]) {
+                    key = false;
+                    break;
+                }
+            }
+        }
+        cout << "Test #" << ++casos << ": ";
+        if(key) cout << "Symmetric." << endl;
+        else cout << "Non-symmetric." << endl;
+        
+    }
+}
