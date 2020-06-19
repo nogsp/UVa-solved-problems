dc357a53d394bd9640982b734b3c9f9d2a30108c
Pedro Nogueira Coutinho <49210182+P-N-C@users.noreply.github.com>
2020-06-18 22:19:58 -0300

    Create 00927 Integer Sequences from Addition of Terms.cpp

---
+++ Complete Search/Iterative (One Loop, Linear Scan)/00927 Integer Sequences from Addition of Terms.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define Matrix vector<vector<ll>>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--) {
+        ll n;
+        cin >> n;
+        vector <ll> vec(n + 1);
+        for(ll i = 0; i < n + 1; i++) {
+            cin >> vec[i];
+        }
+        ll d, k;
+        cin >> d >> k;
+        ll it = 0, itx = 0;
+        while(it < k){
+            it += (d * (++itx));
+        }
+        ll resp = vec[0];
+        for(ll i = 1; i < n + 1; i++) {
+            resp += (vec[i] * pow(itx, i));
+        } 
+        cout << resp << endl;
+    }
+}
